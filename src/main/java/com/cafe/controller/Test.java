62d75b8bc0d657a2b3eaa80f688a2e5f1267feab
Aavegvcs <144206781+Aavegvcs@users.noreply.github.com>
2024-02-14 17:35:42 +0530

    test controller change by aftab

---
+++ src/main/java/com/cafe/controller/Test.java
@@ -0,0 +1,34 @@
+package com.cafe.controller;
+
+import com.cafe.jwt.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @Autowired
+    JwtFilter jwtfilter;
+    @PostMapping("/getTest")
+    String test(){
+        if(jwtfilter.isAdmin()){
+            System.out.println("it is admin");
+            System.out.println("it is admin");System.out.println("it is admin");
+            System.out.println("it is admin");
+            System.out.println("it is admin");
+            System.out.println("it is admin");System.out.println("it is admin");
+
+
+            return "hi i am admin";
+        }else if(jwtfilter.isUser()){
+            return "hi i am user";
+        }else {
+            return "both is there";
+        }
+
+       // return "Hello this is test api";
+    }
+
+
+}
